edc20c948ea681bcf6d2779d0efd08eb19207cc1
diegopenuela <diego.penuela@gmail.com>
2017-07-20 21:01:22 -0500

    debug

--- classifier.py
+++ classifier.py
@@ -10,7 +10,7 @@ from sklearn.model_selection import train_test_split
 
 fruits = pd.read_table('fruit_data_with_colors.txt')
 
-fruits.head()
+print(fruits.head())
 
 # create a mapping from fruit label value to fruit name to make results easier to interpret
 lookup_fruit_name = dict(zip(fruits.fruit_label.unique(), fruits.fruit_name.unique()))   
@@ -28,5 +28,5 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 cmap = cm.get_cmap('gnuplot')
 scatter = pd.scatter_matrix(X_train, c= y_train, marker = 'o', s=40, hist_kwds={'bins':15}, figsize=(9,9), cmap=cmap)
 
-type (cmap)
-type (scatter)
+print(type (cmap))
+print(type (scatter))
