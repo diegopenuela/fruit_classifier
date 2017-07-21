783038c375ca3c4904e8e7cd6dc1aee7d701309b
diegopenuela <diego.penuela@gmail.com>
2017-07-21 03:57:40 -0500

    test matplotlib

--- simple_plot.py
+++ simple_plot.py
@@ -1,8 +1,13 @@
-import matplotlib
-matplotlib.use('Agg')
-
 import matplotlib.pyplot as plt
-plt.ion()
-fig, ax = plt.subplots()
-ax.plot([1,2,3])
+import numpy as np
+
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2*np.pi*t)
+plt.plot(t, s)
+
+plt.xlabel('time (s)')
+plt.ylabel('voltage (mV)')
+plt.title('About as simple as it gets, folks')
+plt.grid(True)
+plt.savefig("test.png")
 plt.show()
