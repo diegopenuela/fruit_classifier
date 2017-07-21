5ba9162dd6ec7b5e141e6a28259fd3b46d21f27e
diegopenuela <diego.penuela@gmail.com>
2017-07-21 08:52:22 -0500

    classify new, previously unseen

--- classifier.py
+++ classifier.py
@@ -55,8 +55,18 @@ knn = KNeighborsClassifier(n_neighbors = 5)
 
 
 ### Train the classifier (fit the estimator) using the training data
-knn.fit(X_train, y_train)
+print (knn.fit(X_train, y_train))
 
 ### Estimate the accuracy of the classifier on future data, using the test data
-knn.score(X_test, y_test)
+print (knn.score(X_test, y_test))
 
+### Use the trained k-NN classifier model to classify new, previously unseen objects
+# first example: a small fruit with mass 20g, width 4.3 cm, height 5.5 cm
+fruit_prediction = knn.predict([[20, 4.3, 5.5]])
+print (lookup_fruit_name[fruit_prediction[0]])
+
+# second example: a larger, elongated fruit with mass 100g, width 6.3 cm, height 8.5 cm
+fruit_prediction = knn.predict([[100, 6.3, 8.5]])
+print (lookup_fruit_name[fruit_prediction[0]])
+
+print ("success...")
