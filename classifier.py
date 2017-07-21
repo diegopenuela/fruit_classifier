90dfe30bf0187970302935bf713becaf78848b81
diegopenuela <diego.penuela@gmail.com>
2017-07-20 20:50:45 -0500

    plotting a scatter matrix

--- classifier.py
+++ classifier.py
@@ -1,3 +1,5 @@
+### Import required modules and load data file
+
 #%matplotlib notebook
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,3 +9,19 @@ from sklearn.model_selection import train_test_split
 fruits = pd.read_table('fruit_data_with_colors.txt')
 
 fruits.head()
+
+# create a mapping from fruit label value to fruit name to make results easier to interpret
+lookup_fruit_name = dict(zip(fruits.fruit_label.unique(), fruits.fruit_name.unique()))   
+lookup_fruit_name
+
+### Examining the data
+
+# plotting a scatter matrix
+from matplotlib import cm
+
+X = fruits[['height', 'width', 'mass', 'color_score']]
+y = fruits['fruit_label']
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+cmap = cm.get_cmap('gnuplot')
+scatter = pd.scatter_matrix(X_train, c= y_train, marker = 'o', s=40, hist_kwds={'bins':15}, figsize=(9,9), cmap=cmap)
