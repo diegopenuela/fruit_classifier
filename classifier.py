4a838c7caa2e4d557d372ff6971a723357fb5832
diegopenuela <diego.penuela@gmail.com>
2017-07-21 09:28:11 -0500

    Debug. def plot_fruit_knn

--- classifier.py
+++ classifier.py
@@ -7,6 +7,57 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+
+
+def plot_fruit_knn(X, y, n_neighbors, weights):
+    X_mat = X[['height', 'width']].as_matrix()
+    y_mat = y.as_matrix()
+
+    # Create color maps
+    cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF','#AFAFAF'])
+    cmap_bold  = ListedColormap(['#FF0000', '#00FF00', '#0000FF','#AFAFAF'])
+
+    clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+    clf.fit(X_mat, y_mat)
+
+    # Plot the decision boundary by assigning a color in the color map
+    # to each mesh point.
+    
+    mesh_step_size = .01  # step size in the mesh
+    plot_symbol_size = 50
+    
+    x_min, x_max = X_mat[:, 0].min() - 1, X_mat[:, 0].max() + 1
+    y_min, y_max = X_mat[:, 1].min() - 1, X_mat[:, 1].max() + 1
+    xx, yy = numpy.meshgrid(numpy.arange(x_min, x_max, mesh_step_size),
+                         numpy.arange(y_min, y_max, mesh_step_size))
+    Z = clf.predict(numpy.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Plot training points
+    plt.scatter(X_mat[:, 0], X_mat[:, 1], s=plot_symbol_size, c=y, cmap=cmap_bold, edgecolor = 'black')
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+
+    patch0 = mpatches.Patch(color='#FF0000', label='apple')
+    patch1 = mpatches.Patch(color='#00FF00', label='mandarin')
+    patch2 = mpatches.Patch(color='#0000FF', label='orange')
+    patch3 = mpatches.Patch(color='#AFAFAF', label='lemon')
+    plt.legend(handles=[patch0, patch1, patch2, patch3])
+
+        
+    plt.xlabel('height (cm)')
+    plt.ylabel('width (cm)')
+    
+    plt.show()
+
+
+
+
+
 fruits = pd.read_table('fruit_data_with_colors.txt')
 
 print(fruits.head())
@@ -70,7 +121,7 @@ fruit_prediction = knn.predict([[100, 6.3, 8.5]])
 print (lookup_fruit_name[fruit_prediction[0]])
 
 ### Plot the decision boundaries of the k-NN classifier
-from adspy_shared_utilities import plot_fruit_knn
+#from adspy_shared_utilities import plot_fruit_knn
 
 plot_fruit_knn(X_train, y_train, 5, 'uniform')   # we choose 5 nearest neighbors
 
