ea65e6bdaa928e81f4c1eb1ecef6c63eed55b37e
diegopenuela <diego.penuela@gmail.com>
2017-07-21 04:41:31 -0500

    second plot

--- classifier.py
+++ classifier.py
@@ -27,4 +27,14 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 cmap = cm.get_cmap('gnuplot')
 scatter = pd.scatter_matrix(X_train, c= y_train, marker = 'o', s=40, hist_kwds={'bins':15}, figsize=(9,9), cmap=cmap)
 
+# plotting a 3D scatter plot
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection = '3d')
+ax.scatter(X_train['width'], X_train['height'], X_train['color_score'], c = y_train, marker = 'o', s=100)
+ax.set_xlabel('width')
+ax.set_ylabel('height')
+ax.set_zlabel('color_score')
+
 plt.show()
