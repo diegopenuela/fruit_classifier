038cfca43de0b97028117e51c14b4cbe1c5847ac
diegopenuela <diego.penuela@gmail.com>
2017-07-21 09:34:16 -0500

    Debug. def plot_fruit_knn

--- classifier.py
+++ classifier.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn import neighbors
 
 
 
